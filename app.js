bb5b1a19c62c94838d9375f486e0b075ab26066f
ANTHONY WOO <anthony.woo14@gmail.com>
2022-12-10 17:45:00 -0700

    cleaned up

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 const nameDisplay = document.getElementById('name-display');
 const button = document.getElementById('updateButton');
-const namebox = document.getElementById('name-input');
-const pronounbox = document.getElementById('pronoun-input');
+const nameBox = document.getElementById('name-input');
+const pronounBox = document.getElementById('pronoun-input');
 const pronounDisplay = document.getElementById('pronouns');
-namebox.addEventListener('keyup', () => {
-    nameDisplay.textContent = namebox.value;
+nameBox.addEventListener('keyup', () => {
+    nameDisplay.textContent = nameBox.value;
 });
 button.addEventListener('click', () => {
-    console.log('i am clicking the button');
-    
     nameDisplay.style.color = 'green';
-    pronounDisplay.textContent = pronounbox.value;
+    pronounDisplay.textContent = pronounBox.value;
 });
