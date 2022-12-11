49b88646ef09743adde40b0124e22195aa5913bf
ANTHONY WOO <anthony.woo14@gmail.com>
2022-12-10 17:15:52 -0700

    stuff done by the end of the demonstration

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
-// grab DOM elements
-
-// set event listeners
+const nameDisplay = document.getElementById('name-display');
+const button = document.getElementById('updateButton');
+const namebox = document.getElementById('name-input');
+const pronounbox = document.getElementById('pronoun-input');
+const pronounDisplay = document.getElementById('pronouns');
+button.addEventListener('click', () => {
+    console.log('i am clicking the button');
+    nameDisplay.textContent = namebox.value;
+    nameDisplay.style.color = 'green';
+    pronounDisplay.textContent = pronounbox.value;
+});
---
+++ dataTypes.js
@@ -0,0 +1,25 @@
+console.log('hello world from app.js');
+
+// data types
+//numbers 
+42;
+//strings
+let answer ='42';
+let dogName ='Cake';
+
+console.log(`My dogs name is ${dogName}`);
+
+
+//arrays - list of things
+let colors = ['green', 'red', 'blue', 'purple'];
+console.log(colors);
+console.log(colors[0]);
+
+// objects - key values
+
+let cake = {
+    name: 'Cake',
+    age: 2, 
+    breeds: ['Corgi', 'Border Collie'],
+};
+console.log(cake);
